6a2439dcd745b5f9ca7f8a418b56eda8599db2eb
이재익 <csljoke@gmail.com>
2022-04-30 02:22:29 +0900

    문자열 압축

--- Level2/문자열 압축/jake.py
+++ Level2/문자열 압축/jake.py
@@ -1,45 +1,38 @@
 # stack
-import sys
-sys.stdin = open('input.txt')
 
-T = int(input())
-for tc in range(1, T+1):
-    s = input()
+def solution(s):
+    # 문자열은 제일 앞부터 정해진 길이만큼 잘라야 합니다.
+    
     answer = len(s)
-    for i in range(2, len(s)):
-        index = 0
-        stack1 = []
-        stack2 = []
-        final = []
-        while index < len(s):
-            print(stack1, stack2)
-            if len(stack1) != i:
-                stack1.append(s[index])
-                index += 1
-            elif len(stack1) == i:
-                stack2.append(s[index])
-                index += 1
-            elif len(stack1) == i and len(stack2) == i:
-                n = ''.join(stack1)
-                m = ''.join(stack2)
-                if n != m:
-                    final.append(n)
-                    final.append(m)
-                    stack1.clear()
-                    stack2.clear()
-                    index += 1
+    # 1개부터, 절반까지 잘라서 생각해봄.
+    for i in range(1, len(s)//2+1):
+        # 새로 만들 단어장
+        press = ''
+        # 1번째부터 i번째까지 계속되는 문자열
+        stack = s[:i]
+        count = 1
+        # 만약 i+1번부터 2i번까지 같다면
+        for j in range(i, len(s), i):
+            # 스택을 더해줌
+            if s[j:j+i] == stack:
+                count += 1
+            # 아니라면 지금까지 쌓은 스택을 새로 만들 단어에 저장해나가야 함
+            else:
+                # count가 2 이상이여야 압축의 의미가 있음
+                if count >= 2:
+                    press = press + str(count) + stack
+                # count가 1이라면 그냥 더해줌
                 else:
-                    final.append('i')
-                    final.append(n)
-                    stack1.clear()
-                    stack2.clear()
-                    index += 1
-            elif index == len(s)-1:
-                final.append(''.join(stack1))
-                final.append(''.join(stack2))
-                stack1.clear()
-                stack2.clear()
-                index+=1
-        if len(''.join(final)) < answer:
-            answer = len(''.join(final))
-    print(answer)
+                    press = press + stack
+                # 새로운 반복 문자열 형성
+                stack = s[j:j+i]
+                count = 1
+        # 비교가 끝나고 남은 것도 더해줌
+        if count >= 2:
+            press = press + str(count) + stack
+        else:
+            press = press + stack
+        # 최소값을 찾는 것이므로 작으면 저장
+        if len(press)<answer:
+            answer = len(press)
+    return answer
